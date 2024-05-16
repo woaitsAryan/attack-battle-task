bfb0f5c0cf8502b600c3aa92319b802d308c80b5
woaitsAryan <yoprince.ab@gmail.com>
2024-05-17 02:53:31 +0530

    feat: added static site generation on main page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,41 +1,8 @@
-'use client'
-
-import { useEffect, useState } from 'react';
-import { PostType } from '../lib/types';
+import { PostType } from '@/lib/types';
 import PostCard from './components/postCard';
-import { useRouter } from 'next/navigation';
-import Toaster from '../lib/toast';
-import backend from '../lib/axios';
 
-const MainPage = () => {
-    const [posts, setPosts] = useState<PostType[]>([]);
-    const router = useRouter()
-    const [page, setPage] = useState<number>(1);
-
-    useEffect(() => {
-        const toaster = Toaster.startLoad('Fetching posts');
-        try {
-            backend.get(`/posts?page=${page}&limit=20`)
-                .then((res) => {
-                    const postData = res.data.posts as PostType[]
-                    if (postData.length === 0) {
-                        Toaster.stopLoad(toaster, "No posts found", 0);
-                        return;
-                    }
-                    Toaster.stopLoad(toaster, "Posts fetched", 1);
-                    setPosts(res.data.posts)
-                }).catch((err) => {
-                    Toaster.stopLoad(toaster, "You are not signed in", 0);
-                    router.push('/login')
-                })
-        } catch (err: any) {
-            Toaster.stopLoad(toaster, "You are not signed in", 0);
-            router.push('/login')
-        }
-    }, [router, page])
-
-    const nextPage = () => setPage(page + 1);
-    const prevPage = () => setPage(page > 1 ? page - 1 : page)
+const MainPage = async () => {
+    const posts = await getData() as PostType[]
 
     return (
         <main className="h-full flex flex-col justify-center items-center w-full">
@@ -49,18 +16,24 @@ const MainPage = () => {
                             <PostCard key={index} post={post} />
                         ))}
                     </div>
-                    <div className="flex justify-between w-full mt-4">
-                        <button onClick={prevPage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Previous
-                        </button>
-                        <button onClick={nextPage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Next
-                        </button>
-                    </div>
                 </div>
             </div>
         </main>
     );
 };
+
+async function getData() {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/posts?page=1&limit=10`)
+        if (!res.ok) {
+            throw new Error('Failed to fetch data')
+        }
+        const posts = (await res.json()).posts
+        return posts
+    } catch (error) {
+        console.error(error)
+        return []
+    }
+}
 
 export default MainPage;
